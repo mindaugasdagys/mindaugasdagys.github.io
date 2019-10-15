0257b23d938e91c998936bf1ef21fbb3a16b178a
mindaugasdagys <admin@mindaugasdagys.com>
2019-10-15 15:16:07 +0300

    add text change

---
+++ mindaugas-dagys-page/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coding <code>app.js</code> please be patient.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit later.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
